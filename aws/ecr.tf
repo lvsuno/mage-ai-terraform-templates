34bf3a61ac9b887b5eac89f730dff71d250b4cd2
Elvis.T <lvsuno@users.noreply.github.com>
2024-08-12 12:42:57 -0400

    Update ecr.tf add force_delete for ecr

--- aws/ecr.tf
+++ aws/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "container_repository" {
   name                 = "${var.app_name}-${var.app_environment}-repository"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
